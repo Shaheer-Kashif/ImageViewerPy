2b6cf0852b6030edb76d2f4b3cb81cd627b97728
Shaheer <shaheerkashif345@gmail.com>
2024-02-03 11:31:32 +0500

    Functionality Added

--- imgviewer.py
+++ imgviewer.py
@@ -8,16 +8,43 @@ max_dimension = 500
 
 imgno = 0
 
-def forward(n):
-    global imagedisplay, button_back,button_forward, imgno
+def forward():
+    global imagedisplay, button_back,button_forward, imgno,button_forward,button_back,button_quit,status
+        
     imagedisplay.grid_forget()
     imgno += 1
-    imagedisplay = Label(image=all_imgs[imgno])
+    imagedisplay = Label(image=final_imgs[imgno])
+    button_back = Button(root, text="<<",command=previous,font="helvetica")
+    button_forward = Button(root, text=">>",command=forward,font="helvetica")
+    status = Label(root, text= "Image "+str(imgno+1)+" of "+str(len(final_imgs)))
+    
+    if imgno == len(final_imgs) - 1:
+        button_forward = Button(root, text=">>",state = DISABLED)
+    
+    
     imagedisplay.grid(row=0,column=0,columnspan=3)
+    status.grid(row=2,column=1)
+    button_back.grid(row=1,column=0)
+    button_forward.grid(row=1,column=2)
     
 
 def previous():
-    return
+    global imagedisplay, button_back,button_forward, imgno,button_forward,button_back,button_quit,status
+    imagedisplay.grid_forget()
+    imgno -= 1
+    imagedisplay = Label(image=final_imgs[imgno])
+    button_back = Button(root, text="<<",command=previous,font="helvetica")
+    button_forward = Button(root, text=">>",command=forward,font="helvetica")
+    status = Label(root, text= "Image "+str(imgno+1)+" of "+str(len(final_imgs)))
+    
+    if imgno == 0:
+        button_back = Button(root, text="<<",state = DISABLED)
+    
+    
+    imagedisplay.grid(row=0,column=0,columnspan=3)
+    status.grid(row=2,column=1)
+    button_back.grid(row=1,column=0)
+    button_forward.grid(row=1,column=2)
 
 img1 = Image.open("images/1.png")
 img2 = Image.open("images/2.png")
@@ -26,6 +53,7 @@ img4 = Image.open("images/4.png")
 imgs = [img1,img2,img3,img4]
 resized_imgs = list()
 
+#image resizing
 for i in imgs:
     width, height = i.size
     resized_image = i
@@ -39,23 +67,24 @@ for i in imgs:
         resized_image = i.resize((int(width), int(height)))
     resized_imgs.append(resized_image)
 
+#image allocator
 my_img1 = ImageTk.PhotoImage(resized_imgs[0])
 my_img2 = ImageTk.PhotoImage(resized_imgs[1])
 my_img3 = ImageTk.PhotoImage(resized_imgs[2])
 my_img4 = ImageTk.PhotoImage(resized_imgs[3])
+final_imgs = [my_img1, my_img2, my_img3, my_img4]
 
-all_imgs = [my_img1,my_img2,my_img3,my_img4]
-
-
-imagedisplay = Label(image=all_imgs[imgno])
+status = Label(root, text= "Image "+str(imgno+1)+" of "+str(len(final_imgs)))
+imagedisplay = Label(image=final_imgs[imgno])
 imagedisplay.grid(row=0,column=0,columnspan=3)
 
-button_back = Button(root, text="<<",command=previous,state=DISABLED)
-button_quit = Button(root, text="Exit",command=root.quit)
-button_forward = Button(root, text=">>",command=lambda: forward(1))
+button_back = Button(root, text="<<",command=previous,state=DISABLED,font="helvetica")
+button_quit = Button(root, text="Exit",command=root.quit,font="helvetica")
+button_forward = Button(root, text=">>",command= forward,font="helvetica")
 
 button_back.grid(row=1,column=0)
 button_quit.grid(row=1,column=1)
 button_forward.grid(row=1,column=2)
+status.grid(row=2,column=1)
 
 root.mainloop()
